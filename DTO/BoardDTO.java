14f7ac97d53e18557b8b77944f70b36dbfbf12f2
sinhyeongi <106128885+sinhyeongi@users.noreply.github.com>
2023-10-04 12:13:45 +0900

    DAO/DTO

---
+++ DTO/BoardDTO.java
@@ -0,0 +1,49 @@
+package DTO;
+
+import java.sql.Date;
+
+public class BoardDTO {
+	private int num;
+	private String subject;
+	private String content;
+	private String name;
+	private String password;
+	private Date signdate;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getSigndate() {
+		return signdate;
+	}
+	public void setSigndate(Date signdate) {
+		this.signdate = signdate;
+	}
+	
+}
